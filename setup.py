44dbadabf8de29a41f9573a10cea53e2fbbbaa4c
Roberto Calandra <roberto.calandra@berkeley.edu>
2016-12-01 14:17:33 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scypyplot',
+      version='0.1',
+      description='A Python Toolbox for Creating Scientific Article Figures',
+      url='https://github.com/robertocalandra/ScyPyPlot',
+      author='Roberto Calandra',
+      author_email='roberto.calandra@berkeley.edu',
+      license='',
+      packages=['scypyplot'],
+      zip_safe=False
+      )
