1a9744493ec2630e1186f28b48166118b880335d
Roberto Calandra <roberto.calandra@berkeley.edu>
2016-12-01 20:04:58 -0800

    more fiddling

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'numpy >= 1.7'
+    ]
+
+dependency_links = [
+    ]
+
+
+def read(fname):
+    return open(os.path.join(BASE_DIR, fname)).read()
 
 setup(name='scypyplot',
-      version='0.1',
+      version='0.0.1dev0',
       description='A Python Toolbox for Creating Scientific Article Figures',
-      url='https://github.com/robertocalandra/ScyPyPlot',
+      url='https://github.com/robertocalandra/scypyplot',
       author='Roberto Calandra',
       author_email='roberto.calandra@berkeley.edu',
-      license='',
-      packages=['scypyplot'],
-      zip_safe=False
+      keywords='visualization',
+      long_description=read('README.md'),
+      license='LICENSE.txt',
+      packages=find_packages(),
+      install_requires=requires,
+      dependency_links=dependency_links,
+      zip_safe=True
       )
