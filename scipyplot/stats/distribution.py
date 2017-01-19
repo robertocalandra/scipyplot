544bcfde6ff83c4cfa8ebcd9506e230a7907cf64
Roberto Calandra <roberto.calandra@berkeley.edu>
2017-01-18 20:16:21 -0800

    support for nan

--- scipyplot/stats/distribution.py
+++ scipyplot/stats/distribution.py
@@ -2,24 +2,30 @@ import numpy as np
 import scipy.stats
 
 __author__ = 'Roberto Calandra'
-__version__ = '0.3'
+__version__ = '0.4'
 
 
 def mean_var(data):
     # TODO: assert is a np.array
-    mean = np.mean(data, axis=0)
-    var = np.var(data, axis=0)
+    mean = np.nanmean(data, axis=0)
+    var = np.nanvar(data, axis=0)
     return [mean, var]
 
 
 def mean_std(data):
     # TODO: assert is a np.array
-    mean = np.mean(data, axis=0)
-    std = np.std(data, axis=0)
+    mean = np.nanmean(data, axis=0)
+    std = np.nanstd(data, axis=0)
     return [mean, std]
 
 
 def mean_percentile(data, des_percentiles='68+95+99'):
+    """
+
+    :param data:
+    :param des_percentiles:
+    :return:
+    """
     mean, variance = mean_var(data=data)
     out = np.array(map(int, des_percentiles.split("+")))
     for i in range(out.size):
@@ -35,18 +41,25 @@ def median_percentile(data, des_percentiles='68+95+99'):
     :param des_percentiles: string with +separated values of the percentiles
     :return:
     """
-    median = np.median(data, axis=0)
+    median = np.nanmedian(data, axis=0)
     out = np.array(map(int, des_percentiles.split("+")))
     for i in range(out.size):
         assert 0 <= out[i] <= 100, 'Percentile must be >0 <100; instead is %f' % out[i]
     list_percentiles = np.empty((2*out.size,), dtype=out.dtype)
     list_percentiles[0::2] = out        # Compute the percentile
     list_percentiles[1::2] = 100 - out  # Compute also the mirror percentile
-    percentiles = np.percentile(data, list_percentiles, axis=0)
+    percentiles = np.nanpercentile(data, list_percentiles, axis=0)
     return [median, percentiles]
 
 
 def percentileFromGaussian(mean, variance, percentile='68+95+99'):
+    """
+
+    :param mean:
+    :param variance:
+    :param percentile:
+    :return:
+    """
     # assert 0 <= percentile <= 100, 'Percentile must be >0 <100; instead is %f' % percentile  # TODO: assert for both numpy.array and single numbers
     a = np.array([0.68, 0.95, 0.99])
     std = np.sqrt(variance)
